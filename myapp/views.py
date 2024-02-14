226bacd1ba37135427b03c5eefc098b4e5ecb960
Stan1-0 <stankofb@gmail.com>
2024-02-14 21:33:25 +0000

    Updated models.py, views.py, and index.html

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html',)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our services are very quick. OnGod'
+    
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service are always on point'
+    
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Afordable'
+    feature3.details = 'Our services are very affordable compared to other retailers'
+    
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Quality'
+    feature4.details = 'We offer you only quality goods and services'
+    return render(request, 'index.html', feature = [feature1, feature2, feature3, feature4])
 
 def counter(request):
     text = request.POST['text']
