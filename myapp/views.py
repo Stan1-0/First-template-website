f80d02011c693ee7d537dcca597459c2b05aaea9
Stan1-0 <stankofb@gmail.com>
2024-02-14 22:55:01 +0000

    Updated models, views, and settings. Added 'myapp' to INSTALLED_APPS. Change template to iterate over 'features' instead of 'feature'.

--- myapp/views.py
+++ myapp/views.py
@@ -23,7 +23,10 @@ def index(request):
     feature4.id = 3
     feature4.name = 'Quality'
     feature4.details = 'We offer you only quality goods and services'
-    return render(request, 'index.html', feature = [feature1, feature2, feature3, feature4])
+    
+    features = [feature1, feature2, feature3, feature4]
+    
+    return render(request, 'index.html', {'features': features} )
 
 def counter(request):
     text = request.POST['text']
