0580f0902ad62dc10d7e83f6abc22e1e62228d54
Stan1-0 <stankofb@gmail.com>
2024-02-15 19:53:58 +0000

    Update features from hard-coded list to database query.

--- myapp/views.py
+++ myapp/views.py
@@ -4,28 +4,7 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.details = 'Our services are very quick. OnGod'
-    
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.details = 'Our service are always on point'
-    
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Afordable'
-    feature3.details = 'Our services are very affordable compared to other retailers'
-    
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Quality'
-    feature4.details = 'We offer you only quality goods and services'
-    
-    features = [feature1, feature2, feature3, feature4]
-    
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features} )
 
 def counter(request):
