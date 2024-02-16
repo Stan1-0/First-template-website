ab88571f7f5363e3b5bab4fb0735e6ca5c024c56
Stan1-0 <stankofb@gmail.com>
2024-02-16 21:13:45 +0000

    Added user registration and authentication functionality.

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Feature
+from  django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -13,4 +15,17 @@ def counter(request):
     return render(request, 'counter.html', {'amount': amount_of_words})
 
 def register(request):
+    if  request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['repeat-password']
+        
+        if password == password2:
+            if User.object.filter(email=email).exists():
+                messages.info(request, 'Email has already been used')
+            
+        
+        
+        
     return render(request, 'register.html')
